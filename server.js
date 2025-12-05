4ba58a17a5bc59e40e3cd2bcea83cabc75a642f3
walid4mp <walid4mco@gmail.com>
2025-12-05 14:38:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {cors:{origin:"*"}});
+const io = require('socket.io')(http, { cors: { origin: "*" } });
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// خدمة الملفات الثابتة (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, '/')));
+
+// أي طلب غير موجود، أرسل index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let rooms = {};
 
-io.on('connection', socket=>{
+io.on('connection', socket => {
     console.log("Player connected:", socket.id);
 
-    socket.on('joinRoom', roomId=>{
-        if(!rooms[roomId]) rooms[roomId] = [];
+    socket.on('joinRoom', roomId => {
+        if (!rooms[roomId]) rooms[roomId] = [];
         rooms[roomId].push(socket.id);
         socket.join(roomId);
         io.to(roomId).emit('players', rooms[roomId]);
     });
 
-    socket.on('updatePaddle', data=>{
-        socket.to(data.room).emit('updatePaddle', {id:socket.id, x:data.x});
+    socket.on('updatePaddle', data => {
+        socket.to(data.room).emit('updatePaddle', { id: socket.id, x: data.x });
     });
 
-    socket.on('ballUpdate', data=>{
+    socket.on('ballUpdate', data => {
         socket.to(data.room).emit('ballUpdate', data);
     });
 
-    socket.on('disconnect', ()=>{
-        for(let room in rooms){
-            rooms[room] = rooms[room].filter(id => id!==socket.id);
+    socket.on('disconnect', () => {
+        for (let room in rooms) {
+            rooms[room] = rooms[room].filter(id => id !== socket.id);
             io.to(room).emit('players', rooms[room]);
         }
         console.log("Player disconnected:", socket.id);
     });
 });
 
-http.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
